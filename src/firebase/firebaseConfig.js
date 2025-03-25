c3529f898440479d585feeddf184854025867612
federicaiglesias <federicaiglesias1@gmail.com>
2025-03-25 11:22:49 +0100

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,13 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "kidflix-9aa45.firebaseapp.com",
-  projectId: "kidflix-9aa45",
-  storageBucket: "kidflix-9aa45.firebasestorage.app",
-  messagingSenderId: "954802525218",
-  appId: "1:954802525218:web:2dace7f31337d9dc34667b",
-  measurementId: "G-ZERRK11B44",
+
 };
 
 const app = initializeApp(firebaseConfig);
